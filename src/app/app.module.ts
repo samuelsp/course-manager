25d2bf614f422184d5c2423f08768118c12c1835
samuelsp <samp_81@yahoo.com.br>
2020-12-28 21:09:41 -0300

    [add] rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { CourseInfoComponent } from './courses/course-info.component';
+import { Error404Component } from './Error404/error-404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,17 +8,36 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'courses', component: CourseListComponent
+        },
+        {
+          path: 'courses/info/:id', component: CourseInfoComponent
+        },
+        {
+          path: '', redirectTo: 'courses', pathMatch: 'full' 
+        },
+        {
+          path: '**', component: Error404Component
+        },
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
