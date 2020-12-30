f2ba931d13c34c02e8f6df7966d51bcdcf607beb
samuelsp <samp_81@yahoo.com.br>
2020-12-30 16:33:17 -0300

    [add] Error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-import { Error404Component } from './Error404/error-404.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './courses/course.module';
@@ -10,12 +8,10 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     CourseModule,
     CoreModule,
@@ -23,10 +19,7 @@ import { CoreModule } from './core/core.module';
       [
         {
           path: '', redirectTo: 'courses', pathMatch: 'full' 
-        },
-        {
-          path: '**', component: Error404Component
-        },
+        }        
       ])
   ],
   providers: [],
