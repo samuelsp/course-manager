5f4366a93c02edfe3a5ed77d02af53ec2920155a
samuelsp <samp_81@yahoo.com.br>
2020-12-30 13:49:58 -0300

    segregando responsabilidade de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,26 @@
-import { CourseInfoComponent } from './courses/course-info.component';
 import { Error404Component } from './Error404/error-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
+    Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot(
       [
-        {
-          path: 'courses', component: CourseListComponent
-        },
-        {
-          path: 'courses/info/:id', component: CourseInfoComponent
-        },
         {
           path: '', redirectTo: 'courses', pathMatch: 'full' 
         },
